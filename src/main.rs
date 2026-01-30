34c97b6e69c31fd4037a63c95395b83b43dff9ac
leirdan <uouleirdan@gmail.com>
2026-01-29 21:46:05 -0300

    [FEAT] Simple main impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use std::fmt::Error;
+use anyhow::Result;
+use log::{debug, info};
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    info!("Hello, world!");
+    debug!("Making request to page...");
+
+    let res = reqwest::get("https://en.uesp.net/wiki/Skyrim:Skyrim")
+        .await?
+        .text()
+        .await?;
+
+    info!("{:?}", res);
 
     Ok(())
 }
